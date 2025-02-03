ab82e40c948a1e5d24e5dae066a0e3258b8b0cb4
sam <sam.how44@gmail.com>
2025-02-03 12:09:25 -0700

    landingsection touch up

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import '../styles/LandingSection.css';
 
 import mountains from '/mountains.jpg';
-// import homeBg from '/home-bg.jpg';
-// import livingBg from '/living-bg.jpg';
 
 const handleNavClick = (sectionId) => {
     const element = document.getElementById(sectionId);
@@ -21,11 +19,8 @@ const LandingSection = () => {
                     <div className="landing-content">
                         <h1>Welcome</h1>
                         <div className="landing-address">
-                            <h2>Here you can find my projects and some other info</h2>
+                            <h2>Sam Howard's Projects and Contact Info</h2>
                         </div>
-                            <button onClick={() => handleNavClick('contact')}>
-                                <span>Contact Me</span>
-                             </button>
                         </div>
                     <div className="landing-images">
                         <img
@@ -33,16 +28,6 @@ const LandingSection = () => {
                             alt="Mountains"
                             className="landing-image"
                         />
-                        {/* <img
-                            src={homeBg}
-                            alt="Elegant interior design"
-                            className="hero-image"
-                        />
-                        <img
-                            src={livingBg}
-                            alt="Serene outdoor living space"
-                            className="hero-image"
-                        /> */}
                     </div>
                 </div>
             </div>
