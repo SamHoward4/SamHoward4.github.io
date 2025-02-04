baeea8d7a45abef4532489837ac3972fb212e887
Sam Howard <73212251+SamHoward4@users.noreply.github.com>
2025-02-04 16:05:11 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensure .jsx files are resolved
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Ensure output files have .js extension
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
 });
