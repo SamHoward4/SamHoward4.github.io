e85c28a7c1e502a6cf167f1a1a44ce144d95750f
sam <sam.how44@gmail.com>
2025-02-04 11:52:07 -0700

    project message touch up

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,10 +4,10 @@ import '../styles/ProjectSection.css';
 const ProjectSection = () => {
     return (
         <section id="projects" className="projects-container">
-            <div className="text">
-                <h1>uh oh!</h1> 
-                <h5>Looks like I'm working on this section, if you'd like to see my projects, contact me </h5>
-                <h5>--- sam (1/30/2025)</h5>
+            <div className="projects-content">
+                <h1>hmmm</h1> 
+                <h1>Looks like I'm working on this section right now, sorry! </h1>
+                <h1>--- sam (2/4/2025)</h1>
             </div>
         </section>
     );
