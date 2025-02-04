c1c883e2c9e380a3a9dc929913c28d1115e4d74b
sam <sam.how44@gmail.com>
2025-02-04 13:27:45 -0700

    info section all content

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import '../styles/InfoSection.css';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { SiLeetcode } from 'react-icons/si';
+import { IconContext } from 'react-icons';
 
 const InfoSection = () => {
     return (
         <section id="info" className="info-container">
             <div className="info-content">
-                <h2>Lorem Ipsum</h2>
+
+                <div className="info-description">
+                    <h3>Hi! I'm Sam Howard,</h3>
+                    <p>and welcome to my little corner of the internet. Here you an find some projects I've worked on and some ways to connect with me. Enjoy!</p>
+                </div>
+
+                <h3>Check out my...</h3>
+                <br></br>
                 <div className="info-grid">
                     <div className="info-item">
-                        <h3>Lorem</h3>
-                        <p>123</p>
+                        <IconContext.Provider value={{size: "3.5em"}}>
+                            <div className="icon">
+                                <FaLinkedin />
+                            </div>
+                        </IconContext.Provider>
+                        <h1>:</h1>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sam-howard-70777321b">Linked In</a>
                     </div>
                     <div className="info-item">
-                        <h3>Ipsum</h3>
-                        <p>456</p>
+                    <IconContext.Provider value={{size: "3.5em"}}>
+                            <div>
+                                <FaGithub />
+                            </div>
+                        </IconContext.Provider>
+                        <h1>:</h1>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/SamHoward4">Github</a>
+
                     </div>
                     <div className="info-item">
-                        <h3>Dolor</h3>
-                        <p>789</p>
-                    </div>
-                    <div className="info-item">
-                        <h3>Sit</h3>
-                        <p>012</p>
+                    <IconContext.Provider value={{size: "3.5em"}}>
+                            <div>
+                                <SiLeetcode/>
+                            </div>
+                        </IconContext.Provider>
+                        <h1>:</h1>
+                        <a target="_blank" rel="noopener noreferrer" href="https://leetcode.com/u/DoctorCow/"> Leetcode</a>
                     </div>
                 </div>
-                <div className="property-description">
-                    <h3>Lorem Ipsum Dolor</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+
             </div>
         </section>
     );
