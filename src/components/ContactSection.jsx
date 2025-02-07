45bf409af70ba077325c80fc5f85bf35f8f3f33f
sam <sam.how44@gmail.com>
2025-02-07 13:59:43 -0700

    contact form functionality

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import '../styles/ContactSection.css';
+import emailjs from "@emailjs/browser";
+
 
 const ContactSection = () => {
+    const form = useRef();
+    const [status, setStatus] = useState("");
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            "service_jbis8kr",
+            "template_stfmpnt",
+            form.current,
+            "tUqfMM-cWw8wMrKlL"
+        )
+        .then(() => {
+            setStatus("Message sent successfully!");
+
+            form.current.reset();
+          })
+          .catch((error) => {
+            setStatus("Failed to send message.");
+            console.error("Error:", error);
+          });
+
+    }
     return (
         <section id="contact" className="contact-container">
             <div className="contact-content">
@@ -12,13 +37,14 @@ const ContactSection = () => {
                         <br></br>
                         <p>email: sam.how44@gmail.com</p>
                     </div>
-                    <form className="contact-form">
-                        <input type="text" placeholder="This" />
-                        <input type="email" placeholder="Doesn't" />
-                        <input type="tel" placeholder="Work" />
-                        <textarea placeholder="Yet..."></textarea>
+                    <form ref={form} onSubmit={sendEmail} className="contact-form">
+                        <input name="from_name" type="text" placeholder="Your Name" required/>
+                        <input name="reply_email" type="email" placeholder="Your Email" required />
+                        <input name="subject" type="text" placeholder="Subject" required />
+                        <textarea name="message" placeholder="Message..." required></textarea>
                         <button type="submit">Send</button>
                     </form>
+                    {status && <p>{status}</p>}
                 </div>
             </div>
         </section>
